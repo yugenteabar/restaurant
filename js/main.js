e33f185d64d9dba73e98ceb9af0be5931d13ddde
Vladimir <vladimir96ks@gmail.com>
2022-08-26 12:25:14 +0200

    add mobile resolution

--- js/main.js
+++ js/main.js
@@ -2,8 +2,32 @@
   const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
   let TOGGLE_MENU = false
 
+  let containerMargin
+
+
+  function defineContainerMargin() {
+    const container = document.querySelector('.container')
+    containerMargin = (innerWidth - container.offsetWidth) / 2
+
+    const cpls =  document.querySelectorAll('.custom-padding-left')
+    const cprs =  document.querySelectorAll('.custom-padding-right')
+
+    cpls.forEach(cpl => {
+      cpl.style.paddingLeft = containerMargin + 'px'
+    })
+    cprs.forEach(cpr => {
+      cpr.style.paddingRight = containerMargin + 'px'
+    })
+  }
+
+
+
+
+
+
   const menuBtn = document.querySelector('.open-navigation-btn')
-  const nav = document.querySelector('#nav-bg')
+  const nav = document.querySelector('#navBg')
+  const closeNavBtn = document.querySelector('#closeNavBtn')
   menuBtn.addEventListener('click', () => {
     if(!TOGGLE_MENU){
       nav.classList.remove('hidden')
@@ -15,10 +39,25 @@
   })
 
   nav.addEventListener('click', (e) => {
-    if(e.target.id === 'nav-bg'){
+    if(e.target.id === 'navBg'){
       nav.classList.add('hidden')
       TOGGLE_MENU = !TOGGLE_MENU
     }
   })
 
+  closeNavBtn.addEventListener('click', (e) => {
+      nav.classList.add('hidden')
+      TOGGLE_MENU = !TOGGLE_MENU
+  })
+
+  addEventListener('keydown', e => {
+    if(!nav.classList.contains('hidden') && (e.key === "Escape" || e.key === "Esc")){
+      nav.classList.add('hidden')
+      TOGGLE_MENU = !TOGGLE_MENU
+    }
+  })
+
+  addEventListener('resize', defineContainerMargin)
+
+  defineContainerMargin()
 })()
